0fea79f067d2156b2a712d4382c6562f0274079a
alex-muliande <alexmuliande@gmail.com>
2019-07-13 18:44:55 +0300

    added switch gender option.

--- script.js
+++ script.js
@@ -6,3 +6,47 @@ let input = function () {
   month = parseInt(document.getElementById("months").value);
   dayOfMonth = parseInt(document.getElementById("day").value);
 }
+function calculate() {
+  input();
+
+  dayOfWeek = (((century / 4) - 2 * century - 1) + ((5 * year / 4)) + ((26 * (month + 1) / 10)) + dayOfMonth) % 7;
+  return (Math.round(dayOfWeek));
+}
+function checkDayOfWeek() {
+  dayNum = calculate();
+  checkGender();
+}
+
+
+
+function checkGender() {
+  var gender = document.getElementsByName("gender");
+  if (gender[0].checked == true) {
+    gender = "male";
+  } else if (gender[1].checked == true) {
+    gender == "female";
+  } else {
+    console.log("Failed")
+  }
+  switch (gender) {
+    case "male":
+      if (dayNum == 1) {
+        //document.getElementById("name").innerHTML = The day is on a sunday." + "  " + "Your akan name is  kwasi";
+        document.write("The day is on a sunday." + "  " + "Your akan name is  kwasi");
+      } else if (dayNum == 2) {
+        document.write("The day is on a monday." + " " + "Your akan name is kwadwo ");
+      } else if (dayNum == 3) {
+        // document.write("The day is on a tuesday." + " " + "Your akan name is kwabena");
+        document.getElementById("name").innerHTML = "The day is on a tuesday. and Your akan name is kwabena";
+      } else if (dayNum == 4) {
+        document.write("The day is on a wednesday." + " " + "Your akan name is kwaku");
+      } else if (dayNum == 5) {
+        document.write("The day is on a thursday." + " " + "Your akan name is yaw");
+      } else if (dayNum == 6) {
+        document.write("The day is on a friday." + " " + "Your akan name is kofi");
+      } else if (dayNum == 7) {
+        document.write("The day is on a saturday." + " " + "Your akan name is kwame");
+      }else{
+        console.log("passed male check");
+      }
+      break
