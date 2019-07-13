63760efa083cb468c01e46c74a3bf772726f7bde
alex-muliande <alexmuliande@gmail.com>
2019-07-13 18:36:54 +0300

    added a variables#.

---
+++ script.js
@@ -0,0 +1,8 @@
+var century, year, month, dayOfMonth, dayOfWeek, dayNum;
+
+let input = function () {
+  century = parseInt(document.getElementById("century").value);
+  year = parseInt(document.getElementById("year").value);
+  month = parseInt(document.getElementById("months").value);
+  dayOfMonth = parseInt(document.getElementById("day").value);
+}
