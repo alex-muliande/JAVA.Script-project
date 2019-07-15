7fdbff93284117b92a256560b13145db3e42b007
alex-muliande <alexmuliande@gmail.com>
2019-07-15 12:37:01 +0300

    added new styling to the html file.

--- script.js
+++ script.js
@@ -1,56 +1,63 @@
-function day (cc,yy,mm,dd){
-  return ((((cc/4)-2*cc-1)+((5*yy/4)) + ((26*(mm+1)/10)) + dd)%7)+1
+function day(cc, yy, mm, dd) {
+  var cc = document.forms[form1][century]
+  var yy = document.forms[form1][year]
+  var mm = document.forms[form1][month]
+  var dd = document.forms[form1][day]
+
+  return (((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7;
+}
+  
+function sub() {
+  var gender = document.form("your gender");
+  var result = day(cc, yy, mm, dd);
+  alert(result);
+
+
+  var males = ["Kwasi", "kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  var females = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+  if (gender == "male" && result == 0) {
+    alert(males[0]);
   }
-  var cc = document.write(prompt("Enter the century"));
-  var yy = document.write(prompt("Enter the year"));
-  var mm = document.write(prompt("Enter the month"));
-  var dd = document.write(prompt("Enter the date of birth"));
-  var gender= prompt("your gender")
-  var result=day(cc,yy,mm,dd).toFixed();
-  var males=["Kwasi","kwadwo","Kwabena","Kwaku","Yaw", "Kofi", "Kwame"];
-  var females=["Akosua","Adwoa","Abenaa","Akua", "Yaa", "Afua", "Ama"];
-  if (gender=="male" && result== 0){
-  alert(males[0]);
+  else if (gender == "male" && result == 1) {
+    alert(males[1]);
   }
-  else if(gender == "male" && result==1){
-  alert(males[1]);
+  else if (gender == "male" && result == 2) {
+    alert(males[2]);
   }
-  else if(gender == "male" && result==2){
-  alert(males[2]);
+  else if (gender == "male" && result == 3) {
+    alert(males[3]);
   }
-  else if(gender == "male" && result==3){
-  alert(males[3]);
+  else if (gender == "male" && result == 4) {
+    alert(males[4]);
   }
-  else if(gender == "male" && result==4){
-  alert(males[4]);
+  else if (gender == "male" && result == 5) {
+    alert(males[5]);
   }
-  else if(gender == "male" && result==5){
-  alert(males[5]);
+  else if (gender == "male" && result == 6) {
+    alert(males[6]);
   }
-  else if(gender == "male" && result==6){
-  alert(males[6]);
+  else if (gender == "female" && result == 0) {
+    alert(females[0]);
   }
-  else if(gender == "female" && result==0){
-  alert(females[0]);
+  else if (gender == "female" && result == 1) {
+    alert(females[1]);
   }
-  else if(gender == "female" && result==1){
-  alert(females[1]);
+  else if (gender == "female" && result == 2) {
+    alert(females[2]);
   }
-  else if(gender == "female" && result==2){
-  alert(females[2]);
+  else if (gender == "female" && result == 3) {
+    alert(females[3]);
   }
-  else if(gender == "female" && result==3){
-  alert(females[3]);
+  else if (gender == "female" && result == 4) {
+    alert(females[4]);
   }
-  else if(gender == "female" && result==4){
-  alert(females[4]);
+  else if (gender == "female" && result == 5) {
+    alert(females[5]);
   }
-  else if(gender == "female" && result==5){
-  alert(females[5]);
+  else if (gender == "female" && result == 6) {
+    alert(females[6]);
   }
-  else if (gender == "female" && result==6){
-  alert(females[6]);
+  else {
+    alert("null");
   }
-  else{
-  alert("null");
-  }
+}
